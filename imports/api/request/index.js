95f6bba4e5cb8090376548fcf72b306946a191bd
Raed Ouerfelli <werfelli.raed@yahoo.com>
2022-09-09 20:04:48 +0100

    login url to env

--- imports/api/request/index.js
+++ imports/api/request/index.js
@@ -1,4 +1,4 @@
 import axios from "axios";
-const BASE_URL = Meteor.settings.private.LOGIN_POST_BASE_URL;
+const BASE_URL = process.env.LOGIN_BASE_URL;
 axios.defaults.baseURL = BASE_URL;
 export default axios
